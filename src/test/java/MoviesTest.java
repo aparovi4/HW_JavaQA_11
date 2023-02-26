0c20ab4cf7b2e3e8cc19b24c1e67faaa0cb85521
Neto Aparovich <aparovich.gregory@gmail.com>
2023-02-26 16:12:38 +0300

    Added test: output of the last 10 movies when exceeding the limit

--- src/test/java/MoviesTest.java
+++ src/test/java/MoviesTest.java
@@ -61,4 +61,22 @@ public class MoviesTest {
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
+    @Test
+    public void findLast11Films() {
+        MoviesManager manager = new MoviesManager();
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+        manager.add("Film 7");
+        manager.add("Film 8");
+        manager.add("Film 9");
+        manager.add("Film 10");
+        manager.add("Film 11");
+        String[] expected = {"Film 11", "Film 10","Film 9","Film 8","Film 7","Film 6","Film 5","Film 4", "Film 3", "Film 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
